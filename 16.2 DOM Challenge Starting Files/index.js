6dbaa06ad58cc17ea682b01aefe3b918463eb29b
artjoms-dev <79485@proton.me>
2024-12-08 15:15:21 +0200

    DOM finish?

--- 16.2 DOM Challenge Starting Files/index.js
+++ 16.2 DOM Challenge Starting Files/index.js
@@ -1,1 +1,7 @@
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "my name";
+document.getElementsByTagName("li")[2].innerHTML = "my name";
+
+document.querySelector("li a").style.color = "red";
+
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge");
